95a2bd5906b67afd2024fb8c5016878c44b75589
Chris Oakman <chris@oakmac.com>
2015-12-22 17:53:41 -0600

    start of JS port

--- parinfer/indent_mode.py
+++ parinfer/indent_mode.py
@@ -1,8 +1,54 @@
-## TODO: write the Indent Mode module here
+## Parinfer.py - a Parinfer implementation in Python
+## https://github.com/oakmac/parinfer.py
 
-def format_text(text, options):
-    ## TODO: implement format_text here ;)
+## More information about Parinfer can be found here:
+## http://shaunlebron.github.io/parinfer/
+
+## Released under the ISC License:
+## https://github.com/oakmac/parinfer.py/blob/master/LICENSE.md
+
+def initial_state():
+    """Returns a dictionary of the initial state."""
     return {
-      'valid': True,
-      'text': text,
+        'lines': [],
+        'insert': {'line_dy': None, 'x': None},
+        'line_no': -1,
+        'quote_danger': False,
+        'track_indent': False,
+        'delim_trail': {'start': None, 'end': None},
+        'stack': [],
+        'backup': [],
+    }
+
+def finalize_state(state):
+    # TODO: write me
+    return state
+
+def process_text(text, options):
+    state = initial_state()
+
+    if options:
+        state['cursor_x'] = options['cursor_x']
+        state['cursor_line'] = options['cursor_line']
+
+    lines = text.split('\n')
+    # TODO: process each line
+
+    # finalize the result
+    state = finalize_state(state)
+
+    # return the state
+    return state
+
+def format_text(in_text, options):
+    state = process_text(in_text, options)
+
+    if state['is_valid'] is True:
+        out_text = '\n'.join(state['lines'])
+    else:
+        out_text = in_text
+
+    return {
+        'valid': True,
+        'text': out_text,
     }
