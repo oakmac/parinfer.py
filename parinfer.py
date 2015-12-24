3475c353ecb1888ee6d1bc748a973b02351337d6
Chris Oakman <chris@oakmac.com>
2015-12-23 20:47:10 -0600

    saving state

--- parinfer.py
+++ parinfer.py
@@ -7,21 +7,251 @@
 ## Released under the ISC License:
 ## https://github.com/oakmac/parinfer.py/blob/master/LICENSE.md
 
+#-------------------------------------------------------------------------------
+# Result Structure
+#-------------------------------------------------------------------------------
+
 def initial_state():
     """Returns a dictionary of the initial state."""
     return {
         'lines': [],
-        'insert': {'line_dy': None, 'x': None},
-        'line_no': -1,
-        'quote_danger': False,
-        'track_indent': False,
-        'delim_trail': {'start': None, 'end': None},
+        'lineNo': -1,
+        'ch': '',
+        'x': 0,
         'stack': [],
         'backup': [],
+        'insert': {'lineNo': None, 'x': None},
+        'parenTrail': {'start': None, 'end': None},
+        'cursorX': None,
+        'cursorLine': None,
+        'cursorDx': None,
+        'quoteDanger': False,
+        'trackIndent': False,
+        'cursorInComment': False,
+        'quit': False,
+        'process': False,
+        'success': False,
+        'maxIndent': None,
+        'indentDelta': 0,
     }
+
+#-------------------------------------------------------------------------------
+# String Operations
+#-------------------------------------------------------------------------------
+
+def insertString(orig, idx, insert):
+    return orig[:idx] + insert + orig[idx:]
+
+def replaceStringRange(orig, start, end, replace):
+    return orig[:start] + replace + orig[end:]
+
+def removeStringRange(orig, start, end):
+    return orig[:start] + orig[end:]
+
+#-------------------------------------------------------------------------------
+# Reader Operations
+#-------------------------------------------------------------------------------
+
+MATCHING_PAREN = {
+    '{': '}',
+    '}': '{',
+    '[': ']',
+    ']': '[',
+    '(': ')',
+    ')': '(',
+}
+
+def isOpenParen(c):
+    return c == "{" or c == "(" or c == "["
+
+def isCloseParen(c):
+    return c == "}" or c == ")" or c == "]"
+
+def isWhitespace(c):
+    return c == " " or c == "\t" or c== "\n"
+
+#-------------------------------------------------------------------------------
+# Stack States
+#-------------------------------------------------------------------------------
+
+def peek(stack, i):
+    if i is None:
+        i = 1
+    return stack[len(stack) - i]
+
+def getPrevCh(stack, i):
+    e = peek(stack, i)
+    return e and e['ch']
+
+def isEscaping(stack):
+    return getPrevCh(stack) == '\\'
+
+def prevNonEscCh(stack):
+    i = 1
+    if isEscaping(stack):
+        i = 2
+    return getPrevCh(stack, i)
+
+def isInStr(stack):
+    return prevNonEscCh(stack) == '"'
+
+def isInComment(stack):
+    return prevNonEscCh(stack) == ';'
+
+def isInCode(stack):
+    return isInStr(stack) != True and isInComment(stack) != True
+
+def isValidCloser(stack, ch):
+    return getPrevCh(stack) == MATCHING_PAREN[ch]
+
+#-------------------------------------------------------------------------------
+# Stack Operations
+#-------------------------------------------------------------------------------
+
+def pushOpen(result):
+    stack = result['stack']
+    if isEscaping(stack) == True:
+        stack.pop()
+        return
+    if isInCode(stack) == True:
+        stack.append({
+            'ch': result['ch'],
+            'indentDelta': result['indentDelta'],
+            'x': result['x'],
+        })
+
+def pushClose(result):
+    stack = result['stack']
+    if isEscaping(stack) == True:
+        stack.pop()
+        return
+
+    backup = result['backup']
+    ch = result['ch']
+    if isInCode(stack) == True:
+        if isValidCloser(stack, ch) == True:
+            opener = stack.pop()
+            result['maxIndent'] = opener['x']
+            backup.append(opener)
+        else:
+            # erase non-matched paren
+            result['ch'] = ""
+
+def pushTab(result):
+    if isInStr(result['stack']) != True:
+        result['ch'] = "  "
+
+def pushSemicolon(result):
+    stack = result['stack']
+    if isEscaping(stack) == True:
+        stack.pop()
+        return
+    if isInCode(stack) == True:
+        stack.append({
+            'ch': result['ch'],
+            'x': result['x'],
+        })
+
+def pushNewline(result):
+    stack = result['stack']
+    if isEscaping(stack) == True:
+        stack.pop()
+    if isInComment(stack) == True:
+        stack.pop()
+    result['ch'] = ""
+
+def pushEscape(result):
+    stack = result['stack']
+    if isEscaping(stack) == True:
+        stack.pop()
+    else:
+        stack.append({
+            'ch': result['ch'],
+            'x': result['x'],
+        })
+
+def pushQuote(result):
+    stack = result['stack']
+    if isEscaping(stack) == True or isInStr(stack) == True:
+        stack.pop()
+        return
+    if isInComment(stack) == True:
+        result['quoteDanger'] = not result['quoteDanger']
+        return
+    # default case
+    stack.append({
+        'ch': result['ch'],
+        'x': result['x'],
+    })
+
+def pushDefault(result):
+    stack = result['stack']
+    if isEscaping(stack) == True:
+        stack.pop()
+
+def pushChar(result):
+    ch = result['ch']
+    if isOpenParen(ch) == True:
+        pushOpen(result)
+        return
+    if isCloseParen(ch) == True:
+        pushClose(result)
+        return
+    if ch == '\t':
+        pushTab(result)
+        return
+    if ch == ';':
+        pushSemicolon(result)
+        return
+    if ch == "\n":
+        pushNewline(result)
+        return
+    if ch == "\\":
+        pushEscape(result)
+        return
+    if ch == '"':
+        pushQuote(result)
+        return
+    # default case
+    pushDefault(result)
+
+#-------------------------------------------------------------------------------
+# Indent Mode Operations
+#-------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------
+# XYZ
+#-------------------------------------------------------------------------------
 
 def finalize_state(state):
     # TODO: write me
+    return state
+
+def process_line(state, line):
+
     return state
 
 def process_text(text, options):
@@ -32,7 +262,8 @@ def process_text(text, options):
         state['cursor_line'] = options['cursor_line']
 
     lines = text.split('\n')
-    # TODO: process each line
+    for line in lines:
+        process_line()
 
     # finalize the result
     state = finalize_state(state)
